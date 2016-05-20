c43edb63b21209b0421f81666b73ba2d1dc04d97
Gerald Png <geraldpng@gmail.com>
2016-05-20 17:13:48 +0100

    Working example with random movement

--- RLI_v1.js
+++ RLI_v1.js
@@ -4,6 +4,7 @@ var gazebo = new gazebojs.Gazebo();
 // Read JSON specs file into obj
 var obj = JSON.parse(fs.readFileSync('specs.json', 'utf8'));
 gazebo.publish(obj.actions[1][0].type, obj.actions[1][0].topic, obj.actions[1][0].msg);	
+gazebo.publish('gazebo.msgs.Physics', '~/physics', { real_time_update_rate: 1});
 console.log("Dummy action");
 
 // Creating http server on port 8124
@@ -37,11 +38,40 @@ http.createServer(function(req, res)
 		{
 			gazebo.publish(obj.actions[select][i].type, obj.actions[select][i].topic, obj.actions[select][i].msg);
 		}
+		res.end("Response end");
+	}
+	if (type == "reset")
+	{
+		// Reset world
+		gazebo.publish("gazebo.msgs.WorldControl", "~/world_control", { reset: { all: true } });
+		// Default action (set as 0 velocity -- car not moving)
+		for (var i = 0; i < obj.actions[0].length; i++)
+		{
+			gazebo.publish(obj.actions[0][i].type, obj.actions[0][i].topic, obj.actions[0][i].msg);
+		}
+		res.end("Response end");
 	}
 
+	if (type == "pause")
+	{
+		if (select == 1)
+			gazebo.publish('gazebo.msgs.WorldControl', '~/world_control', { pause: true});
+		else
+		{
+			gazebo.publish('gazebo.msgs.WorldControl', '~/world_control', { pause: false});
+		}
+		res.end("Response end");
+	}
 	if (action == '/favicon.ico')
 	{
 		console.log('favicon.ico requested -> ignore');
+		res.end("Response end");
+	}
+	if (type == "real_time_factor")
+	{	
+		console.log('helloworld')
+		gazebo.publish('gazebo.msgs.Physics', '~/physics', { real_time_update_rate: select/0.001});
+		res.end('Real time factor')
 	}
 }).listen(8124);
 console.log('Server running at http://localhost:8124/');
